7243ade6da485da4c365bccb4dc11260a71dc866
Yusuf Eser <yusufeser446@gmail.com>
2022-05-01 02:33:53 +0300

    Update validateRoom.js
    - Added Teacher status condition to validate room

--- sockethandlers/validateRoom.js
+++ sockethandlers/validateRoom.js
@@ -2,8 +2,14 @@ module.exports = (io, socket, MyCalls) => {
     socket.on("validateRoom", (roomId) => {
         const call = MyCalls.calls.find(call => call.id == roomId)
   
-        if (call) {
-          socket.emit("validateRoom", call.users.length < 2 ? true : "full", roomId)
+        if (call.users.length < 2) {
+          socket.emit("validateRoom", true, roomId)
+        }
+        else if (call.users.length >= 2) {
+          socket.emit("validateRoom", "full", roomId)
+        }
+        else if (call.teacherStatus === "dnd" || call.teacherStatus === "idle") {
+          socket.emit("validateRoom", "teacherUnavailable", roomId)
         }
         else {
           socket.emit("validateRoom", false)
